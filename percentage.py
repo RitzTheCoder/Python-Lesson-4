9dcfa488c94991f393cc3d57fef84cc738a98e95
RitzTheCoder <ritzthecoder@gmail.com>
2025-03-09 11:58:37 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,8 @@
+mat=int(input("Enter your Math score:"))
+eng=int(input("Enter your English score:"))
+sci=int(input("Enter your Science score:"))     
+tam=int(input("Enter your Tamil score:")) 
+sum = mat+eng+sci+tam
+print("The sum of the given scores=",sum)
+percent=sum/400*100
+print("The overall Percentage =",percent)
